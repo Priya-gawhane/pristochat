a6e9d14f66dd6a0ef99291009ff592631f7b15e5
Priya-gawhane <priyagawhane75@gmail.com>
2026-04-03 18:40:35 +0530

    uppdated

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL;
-
 const API = axios.create({
-    baseURL: baseURL,
-    withCredentials: true
+    baseURL: "/api",
+    withCredentials: true,
 });
 
 export default API;
